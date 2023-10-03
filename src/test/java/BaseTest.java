0c5e02b94ab80b5f35df15b0217580262a2d5126
Denis Karasev <dedaka@yandex.ru>
2023-10-04 02:06:47 +0300

    готовый проект 4 спринта

---
+++ src/test/java/BaseTest.java
@@ -0,0 +1,32 @@
+import static constants.Urls.*;
+import static constants.Timeouts.*;
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import pageobject.MainPage;
+
+import java.time.Duration;
+
+public class BaseTest {
+    protected WebDriver driver;
+
+    @Before
+    public void startUp(){
+
+        driver = new ChromeDriver();
+       // driver = new FirefoxDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(IMPLICITLY_WAIT));
+        driver.get(MAIN_PAGE_URL);
+        driver.manage().window().maximize();
+        MainPage mainPage = new MainPage(driver);
+        mainPage.cookieButtonClick();
+
+    }
+
+    @After
+    public void tearDown(){
+        driver.quit();
+    }
+
+}
