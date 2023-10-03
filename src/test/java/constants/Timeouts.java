f05441f87c70e84175aa69f249d7ab44e5393a54
Dedaka8 <144538592+Dedaka8@users.noreply.github.com>
2023-10-04 02:28:32 +0300

    Delete src directory

--- src/test/java/constants/Timeouts.java
+++
@@ -1,5 +0,0 @@
-package constants;
-
-public class Timeouts {
-    public static final int IMPLICITLY_WAIT = 7;
-}
--- src/test/java/constants/Urls.java
+++
@@ -1,5 +0,0 @@
-package constants;
-
-public class Urls {
-    public static final String MAIN_PAGE_URL = "https://qa-scooter.praktikum-services.ru/";
-}
